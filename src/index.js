b289189e053d2ee2ed9d8cfa1f67effa0765c1f1
haudacia <hadassa.codes@gmail.com>
2024-05-26 09:42:18 +0200

    WIP fix url Render

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.use('/', router);
 
 connectDB().then(() => console.log('Connected to database!'));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT
 
 const server = app.listen(port, () => {
     console.log('Server is up and running in port 3000⚡');
