774477973dd2e174bf4d556c723c06eda35423f0
haudacia <hadassa.codes@gmail.com>
2024-05-27 03:15:31 +0200

    WIP fix url Render

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ app.options('*', cors({
 // Middleware para definir cabeçalhos CORS manualmente, se necessário
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", allowedOrigins);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Credentials", "true");
     next();
