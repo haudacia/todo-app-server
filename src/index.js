867cdb30c997783579298d258c201ed5ffd8654e
haudacia <hadassa.codes@gmail.com>
2024-05-27 03:10:15 +0200

    WIP fix url Render

--- src/index.js
+++ src/index.js
@@ -7,31 +7,33 @@ const mongo = require('./data_mongo/index');
 const app = express();
 app.use(express.json());
 // Allow specific origin(s)
-app.use(cors(
-    {
-        origin: ["https://todo-app-server-cc9x.onrender.com"],
-        methods: ["POST", "GET", "PATCH", "DELETE"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-        credentials: true
+// Configuração do CORS
+const allowedOrigins = ["https://todo-app-3brv.onrender.com"];
 
-    }
-));
+app.use(cors({
+    origin: allowedOrigins,
+    methods: ["POST", "GET", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+}));
 
+// Middleware para lidar com requisições preflight
+app.options('*', cors({
+    origin: allowedOrigins,
+    methods: ["POST", "GET", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+}));
+
+// Middleware para definir cabeçalhos CORS manualmente, se necessário
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://todo-app-3brv.onrender.com");
+    res.header("Access-Control-Allow-Origin", allowedOrigins);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Credentials", "true");
     next();
 });
-// Middleware para lidar com requisições preflight (??)
-app.options('*', cors({
-    origin: ["https://todo-app-server-cc9x.onrender.com"],//url render backend
-    methods: ["POST", "GET", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
 
-}));
 app.use('/', router);
 
 connectDB().then(() => console.log('Connected to database!'));
