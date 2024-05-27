cdf39df921b345b4c654aa468334852d397f55de
haudacia <hadassa.codes@gmail.com>
2024-05-27 02:31:10 +0200

    WIP fix url Render

--- src/controllers/tasks.js
+++ src/controllers/tasks.js
@@ -3,14 +3,19 @@ const Task = require('../data_mongo/schemas/task');
 const getTasks = async (req, res) => {
   try {
     const tasks = await Task.find();
-    res.json(tasks);
+    // Convertendo ObjectId para string
+    const tasksWithStringId = tasks.map(task => ({
+      ...task.toObject(),
+      _id: task._id.toString()
+    }));
+    res.json(tasksWithStringId);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
 const getTask = async (req, res) => {
-  const requestedId = parseInt(req.params.id);
+  const requestedId = parseInt(req.params._id);
   try {
     const task = await Task.findById(requestedId);
     if (task) {
@@ -39,7 +44,7 @@ const createTask = async (req, res) => {
 };
 
 const updateTask = async (req, res) => {
-  const requestedId = parseInt(req.params.id);
+  const requestedId = parseInt(req.params._id);
   const body = req.body;
   try {
     const updatedTask = await Task.findByIdAndUpdate(requestedId, body, { new: true });
@@ -54,7 +59,7 @@ const updateTask = async (req, res) => {
 };
 
 const deleteTask = async (req, res) => {
-  const requestedId = parseInt(req.params.id);
+  const requestedId = parseInt(req.params._id);
   try {
     const deletedTask = await Task.findByIdAndDelete(requestedId);
     if (deletedTask) {
